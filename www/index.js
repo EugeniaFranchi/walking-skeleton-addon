6d8d9ae3a51d8a9afddf0f8c4967d89119a92ba4
Euge <mfranchi@fi.uba.ar>
2022-10-02 19:54:15 -0300

    :)

--- www/index.js
+++ www/index.js
@@ -1,3 +1,3 @@
-import * as wasm from "wasm-game-of-life";
-
-wasm.greet('persona');
+document.body.style.border = "5px solid red";
+// await wasm_bindgen(browser.runtime.getURL("/home/euge/Documentos/TPP/wasm-game-of-life/pkg/wasm_game_of_life_bg.wasm"));
+// wasm_bindgen.greet("persona")
