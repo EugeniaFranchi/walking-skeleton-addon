9033240122b589c09642d82230f7a0551cfb8570
Victor Bravo <vbravo@fi.uba.ar>
2022-11-14 23:07:06 -0300

    Center front table

--- control/frontend/src/App.js
+++ control/frontend/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import axios from 'axios';
 import './App.css';
 import * as React from 'react';
@@ -9,6 +8,8 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import Image from './imgs/main_background.jpg';
+import { Box } from '@mui/material';
 
 
 function App() {
@@ -17,9 +18,9 @@ function App() {
   React.useEffect(() => {
     axios.get("http://localhost:5000/counter").then(res => {
       let r = []
-      Object.keys(res.data).forEach(function(key, index) {
+      Object.keys(res.data).forEach(function (key, index) {
         let s = { site: key, stego: res.data[key].stego, total: res.data[key].total }
-        r.push(s); 
+        r.push(s);
       });
       setData(r)
     }).then(err => {
@@ -29,37 +30,40 @@ function App() {
 
   const BasicTable = () => {
     return (
-      <TableContainer component={Paper}>
-        <Table sx={{ maxWidth: 500 }} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell>Site</TableCell>
-              <TableCell align="right">Stego</TableCell>
-              <TableCell align="right">Total</TableCell>
-              <TableCell align="right">%</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {data.map((row) => (
-              <TableRow
-                key={row.site}
-                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-              >
-                <TableCell align="right">{row.site}</TableCell>
-                <TableCell align="right">{row.stego}</TableCell>
-                <TableCell align="right">{row.total}</TableCell>
-                <TableCell align="right">{row.stego / row.total}</TableCell>
+      <Box sx={{ backgroundImage: `url(${Image})`, height: "100vh", backgroundSize: "cover", display: 'flex', alignItems: 'center' }}>
+        <TableContainer style={{ maxWidth: '100vh', margin: 'auto' }} component={Paper}>
+          <Table aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell><b>Site</b></TableCell>
+                <TableCell align="right"><b>Stego</b></TableCell>
+                <TableCell align="right"><b>Total</b></TableCell>
+                <TableCell align="right"><b>%</b></TableCell>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+            </TableHead>
+            <TableBody>
+              {data.map((row) => (
+                <TableRow
+                  key={row.site}
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                  <TableCell align="right">{row.site}</TableCell>
+                  <TableCell align="right">{row.stego}</TableCell>
+                  <TableCell align="right">{row.total}</TableCell>
+                  <TableCell align="right">{row.stego / row.total}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Box>
+
     );
   }
 
   return (
     <div>
-      <BasicTable/>
+      <BasicTable />
     </div>
   );
 }
