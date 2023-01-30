a269ca768213e06dd58c67af9995599bfb339582
root <mfranchi@fi.uba.ar>
2023-01-30 00:32:26 +0000

    async

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,22 @@
 mod utils;
 
 use wasm_bindgen::prelude::*;
+use wasm_bindgen_futures::JsFuture;
 #[cfg(target_arch = "wasm32")]
-use wasm_thread as thread;
-
 use std::time::Duration;
+use async_std::task;
+use futures::join;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
 #[cfg(feature = "wee_alloc")]
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[wasm_bindgen(module = "/www/bootstrap.js")]
+extern "C" {
+    fn predict(i: i32) -> JsValue;
+}
 
 #[wasm_bindgen]
 extern {
@@ -34,27 +40,29 @@ pub fn add(x: i32, y: i32) -> i32 {
     x + y
 }
 
-#[wasm_bindgen]
-pub fn process() {
-    console_log!("ENTRA A PROCESS");
-    for _ in 0..2 {
-        thread::spawn(|| {
-            for i in 1..3 {
-                console_log!(
-                    "hi number {} from the spawned thread {:?}!",
-                    i,
-                    thread::current().id()
-                );
-                thread::sleep(Duration::from_millis(1));
-            }
-        });
-    }
+async fn hello() -> String {
+    console_log!("start HELLO");
+    task::sleep(Duration::from_secs(5)).await;
+    console_log!("HELLO");
+    String::from("Hello")
+}
 
-    for i in 1..3 {
-        console_log!(
-            "hi number {} from the main thread {:?}!",
-            i,
-            thread::current().id()
-        );
-    }
+async fn world() -> String {
+    console_log!("start WORLD");
+    task::sleep(Duration::from_secs(1)).await;
+    console_log!("WORLD");
+    String::from("World")
+}
+
+#[wasm_bindgen]
+pub async fn process(length: i32) -> String {
+    // Llama a JS para la predicción
+    // https://stackoverflow.com/questions/71217860/how-to-call-an-async-javascript-import-function-from-webassembly-rust-in-a-nod
+    // let a = JsFuture::from(js_sys::Promise::from(predict(0)));
+    // let b = JsFuture::from(js_sys::Promise::from(predict(1)));
+    // let c = JsFuture::from(js_sys::Promise::from(predict(2)));
+    // join!(a,b,c);
+
+    let (h,w) = join!(hello(), world());
+    h + w.as_str()
 }
--- www/bootstrap.js
+++ www/bootstrap.js
@@ -3,54 +3,82 @@ import getPixels from 'get-pixels';
 import util from 'util';
 import axios from 'axios';
 const { process } = wasm_bindgen;
+const images = document.images;
+const asyncGetPixels = util.promisify(getPixels);
 
 document.body.style.border = "5px solid red";
 console.log('from bootstrap')
 
-async function getImagesAndPredict(model) {
-    let stego = 0;
-    let images = document.images
-    const asyncGetPixels = util.promisify(getPixels);
-    console.log("Nro de imágenes a procesar:", images.length);
-    for (var i = 0; i < images.length; i++) {
-        console.log(`Procesando imagen ${i}`);
-        // OBTENER IMAGEN
-        const img = images[i];
-        let pixels = await asyncGetPixels(images[i].src, (err, pixels) => pixels)
+export async function predict(index) {
+    console.log(`Procesando imagen ${index}`);
+    // OBTENER IMAGEN
+    const img = images[index];
+    let pixels = await asyncGetPixels(images[index].src, (err, pixels) => pixels)
 
-        // PREDICCIÓN
-        // Uint8Array -> Tensor 3D RGBA [x,y,4]
-        const rgbaTens3d = tf.tensor3d(pixels, [img.width, img.height, 4])
-        // Tensor 3D RGBA [x,y,4] -> Tensor 3D RGB [x,y,3]
-        const rgbTens3d = tf.slice3d(rgbaTens3d, [0, 0, 0], [-1, -1, 3])
-        // Tensor 3D RGB [x,y,3] -> Tensor 3D RGB [100,100,3]
-        const smallImg = tf.image.resizeBilinear(rgbTens3d, [100, 100]);
-        // Tensor 3D RGB [100,100,3] -> Tensor 4D RGB [1,100,100,3]
-        const tensor = smallImg.reshape([1, 100, 100, 3])
-        const prediction = model.predict(tensor).dataSync()[0];
-        console.log("Prediction: ", prediction);
+    // PREDICCIÓN
+    // Uint8Array -> Tensor 3D RGBA [x,y,4]
+    const rgbaTens3d = tf.tensor3d(pixels, [img.width, img.height, 4])
+    // Tensor 3D RGBA [x,y,4] -> Tensor 3D RGB [x,y,3]
+    const rgbTens3d = tf.slice3d(rgbaTens3d, [0, 0, 0], [-1, -1, 3])
+    // Tensor 3D RGB [x,y,3] -> Tensor 3D RGB [100,100,3]
+    const smallImg = tf.image.resizeBilinear(rgbTens3d, [100, 100]);
+    // Tensor 3D RGB [100,100,3] -> Tensor 4D RGB [1,100,100,3]
+    const tensor = smallImg.reshape([1, 100, 100, 3])
+    // TODO const prediction = model.predict(tensor).dataSync()[0];
+    console.log("Prediction: ", prediction);
 
-        // PINTAR IMAGEN
-        if (prediction >= 0.7) {
-            images[i].style.filter = "blur(2px) grayscale(100%) brightness(40%) sepia(100%) hue-rotate(-50deg) saturate(600%) contrast(0.8)";
-            stego += 1;
-        }
+    // PINTAR IMAGEN
+    if (prediction >= 0.7) {
+        images[i].style.filter = "blur(2px) grayscale(100%) brightness(40%) sepia(100%) hue-rotate(-50deg) saturate(600%) contrast(0.8)";
     }
-    return stego
 }
+
+// async function getImagesAndPredict(model) {
+//     let stego = 0;
+//     let images = document.images
+//     const asyncGetPixels = util.promisify(getPixels);
+//     console.log("Nro de imágenes a procesar:", images.length);
+//     for (var i = 0; i < images.length; i++) {
+//         console.log(`Procesando imagen ${i}`);
+//         // OBTENER IMAGEN
+//         const img = images[i];
+//         let pixels = await asyncGetPixels(images[i].src, (err, pixels) => pixels)
+
+//         // PREDICCIÓN
+//         // Uint8Array -> Tensor 3D RGBA [x,y,4]
+//         const rgbaTens3d = tf.tensor3d(pixels, [img.width, img.height, 4])
+//         // Tensor 3D RGBA [x,y,4] -> Tensor 3D RGB [x,y,3]
+//         const rgbTens3d = tf.slice3d(rgbaTens3d, [0, 0, 0], [-1, -1, 3])
+//         // Tensor 3D RGB [x,y,3] -> Tensor 3D RGB [100,100,3]
+//         const smallImg = tf.image.resizeBilinear(rgbTens3d, [100, 100]);
+//         // Tensor 3D RGB [100,100,3] -> Tensor 4D RGB [1,100,100,3]
+//         const tensor = smallImg.reshape([1, 100, 100, 3])
+//         const prediction = model.predict(tensor).dataSync()[0];
+//         console.log("Prediction: ", prediction);
+
+//         // PINTAR IMAGEN
+//         if (prediction >= 0.7) {
+//             images[i].style.filter = "blur(2px) grayscale(100%) brightness(40%) sepia(100%) hue-rotate(-50deg) saturate(600%) contrast(0.8)";
+//             stego += 1;
+//         }
+//     }
+//     return stego
+// }
 
 async function run() {
     await wasm_bindgen(browser.runtime.getURL('pkg/wasmaddon_bg.wasm'));
 
     // Carga del modelo
-    //const model = await tf.loadLayersModel(browser.runtime.getURL('classifier/model/model.json'));
-    //model.summary();
+    // const model = await tf.loadLayersModel(browser.runtime.getURL('classifier/model/model.json'));
+    // model.summary();
 
 
     console.log("--INICIO PROCESO: IMAGEN -> PREDICCIÓN--");
     //let stego = await getImagesAndPredict(model);
     console.log("--FIN PROCESO--");
-    process()
+
+    console.log("Nro de imágenes a procesar:", images.length);
+    await process(images.length);
 
     console.log(`sending counter ${stego} from page ${document.domain}`);
     await axios.post('http://localhost:5000/counter', { site: document.domain, stego: stego, total: document.images.length })
